3fb1eac0de6eb2e5c70667e81e66052beab422a8
marvelmayco <108984569+marvelmayco@users.noreply.github.com>
2024-03-21 02:07:31 +0530

    Update aesport.py

--- services/aesport.py
+++ services/aesport.py
@@ -17,17 +17,19 @@ class AESport(BaseService):
 
         sections_divs = soup.select(".section-focus")
         for section_div in sections_divs:
-            #ch_link = section_div.find("a")['href']
-            channels_divs = section_div.select(".tv-item")
+            group = section_div.select_one(".head-bar .left").text.strip()
+            channels_divs = section_div.select(".content a")
             for channel_div in channels_divs:
+                name = channel_div.select_one(".channel-name").text.strip()
+                logo = channel_div.select_one("img.hide").get("src")
+                stream_url = channel_div.get('href')
                 channels_data.append({
-                    "name": channel_div.select_one("div.channel-name").text.strip(),
-                    "logo": channel_div.select_one("img.hide").get("src"),
-                    "group": section_div.select_one("div.left").text.strip(),
-                    "stream-url": section_div.select_one("div.content a")['href'],
-                    #"stream-url": channel_div.select_one("img.preview-tv").get("src").replace("preview.jpg", "index.m3u8"),
+                    "name": name,
+                    "logo": logo,
+                    "group": group,
+                    "stream-url": stream_url,
                     "headers": {
-                        "referer": get_base_url(channel_div.parent.get("href")) + "/",
+                        "referer": get_base_url(stream_url) + "/",
                         "user-agent": self.USER_AGENT
                     }
                 })
